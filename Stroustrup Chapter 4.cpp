7f17e10994eb9b061706a4961d395ae85743ebb0
babslabs <ryanbruce99@gmail.com>
2015-11-27 00:49:03 -0500

    added program

--- Stroustrup Chapter 4.cpp
+++ Stroustrup Chapter 4.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
-#include <vector>
 #include <string>
 
 using namespace std;
 int main() {
-	
+	bool done = false;
+	while(done == false){
+		int temp1 = 0;
+		int temp2 = 0;
+		cin >> temp1 >> temp2;
+		cout << "\n" << temp1 << " " << temp2 << "\n";
+	}
+return 0;
 }
