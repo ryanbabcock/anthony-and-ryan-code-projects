98e0e9fd1420491b511907d92253dcd4d8184890
babslabs <ryanbruce99@gmail.com>
2016-01-25 17:00:50 -0500

    changed comment

--- c4/c4e4.cpp
+++ c4/c4e4.cpp
@@ -14,7 +14,7 @@ void guesser(int max, int min, int we_are_on){
 		else
 			cout << "Bad Input!\n";
 		cout << "Max: "<<max << " Min: " << min << " Running Guess: " << we_are_on;
-		we_are_on= min + ((max-min)/2 );//add 1 to round integer division (integer division alway truncates, rememeber this)
+		we_are_on= min + ((max-min)/2 );//(integer division alway truncates, rememeber this)
 		cout << "\nIs your number less than " << we_are_on << '?';
 		if (max - min <= 1){
 			guess = we_are_on;
